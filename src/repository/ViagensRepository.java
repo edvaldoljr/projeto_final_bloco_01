6e2a225c3f04a16c8fdaff5f4a6ec12d82f0c42c
edvaldoljr <edvaldoluizj@gmail.com>
2024-06-20 13:21:04 -0300

    clase Repository

---
+++ src/repository/ViagensRepository.java
@@ -0,0 +1,11 @@
+package repository;
+
+public interface ViagensRepository {
+
+    public void agendarViagemNacional();
+    public void agendarViagemInternacional();
+    public void listarReservasNacionais();
+    public void listarReservasInternacionais();
+    public void CancelarReservaNacional();
+    public void CancelarReservaInternacional();
+}
